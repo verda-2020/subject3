22d8d1a639b83a591127050dd78d54f02fafee0d
verda-2020 <msufuk@gmail.com>
2022-03-04 04:17:02 -0500

    subject1

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+       System.out.println( "  +\"\"\"\"\"\"+");
+       System.out.println( "[|  o o  |]");
+       System.out.println( " |   ^   |" );
+       System.out.println( " |  '-'  |");
+       System.out.println( " |  '-'  |");
+       System.out.println(" +-------+" );
+
+    }
+}
